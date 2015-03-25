2649af33c442dd3a38aec0a0dff220c2350e2c67
Alisson <alissonplus@msn.com>
2015-03-25 19:48:38 -0300

    Corrigido a gravacao do programa e adicionado as validações dos campos obrigatórios

--- server/methods.js
+++ server/methods.js
@@ -1,23 +1,28 @@
+/*
+  111 => Dados vindo do formulario, sempre colocar no inicio do array data no cliente
+*/
 Meteor.methods({
 
-  'dataKey': function(){
+  /*'dataKey': function(){
     return ((1 + Math.floor(1e15)) + new Date().getSeconds(7)).toString(36).toUpperCase();
-  },
+  },*/
 
   'dateNow': function(){
-      var dateObj = new Date();
-      return dateObj.getDate() + '/' + (dateObj.getMonth()+1) + '/' + dateObj.getFullYear();
+    var dateObj = new Date();
+    return dateObj.getDate() + '/' + (dateObj.getMonth()+1) + '/' + dateObj.getFullYear();
+  },
+
+  'validateProgram': function(data){
+    if(data[1] === '' || data[2] === '' || data[4] === ''){
+      return false;//Meteor.throwError('Preencha os campos obrigatórios.');
+    }else{
+      return true;
+    }
   },
 
   'insertProgram': function(data){
-    //if(dataKey() === data[0]){
-      //if(data[1] === '' || data[2] === '' || data[4] === ''){
-      //  Meteor.throwError('Preencha os campos obrigatórios.');
-      //}else{
-        var dateObj = new Date();
-        var dateNow = dateObj.getDate() + '/' + (dateObj.getMonth()+1) + '/' + dateObj.getFullYear();
-        Program.insert({status:1, category_id:data[1], name:data[2], description:data[3], logo:data[4], user_record:1, user_change:1, date_record:dateNow, date_change:dateNow});
-      //}
-    //}
+    if(data[0] === 111 && Meteor.call('validateProgram', data)){
+      Program.insert({status:1, category_id:data[1], name:data[2], description:data[3], logo:data[4], user_record:1, user_change:1, date_record:Meteor.call('dateNow'), date_change:Meteor.call('dateNow')});
+    }
   }
 });
