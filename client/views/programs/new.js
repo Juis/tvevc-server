491e21d1c67d756b017680157023c2be7ed634ca
Alisson <alissonplus@msn.com>
2015-03-25 19:49:27 -0300

    Adicionado a validação do size maior de 300kb não efetua o upload

--- client/views/programs/new.js
+++ client/views/programs/new.js
@@ -8,24 +8,26 @@ Template.programNew.events({
 		//	throwError('Preencha os campos obrigatórios.');
 		//}else{
 			// lembrar pegar e validar a imagem pelo size, clientHeight, clientWidth
-			var data = [Meteor.call('dataKey'), form.target.ownerDocument.all.program[1].value, form.target.ownerDocument.all.program[2].value, form.target.ownerDocument.all.program[3].value, Session.get('imgBase64')];
-			Meteor.call('insertProgram', data);
+			Meteor.call('insertProgram', [111, form.target.ownerDocument.all.program[1].value, form.target.ownerDocument.all.program[2].value, form.target.ownerDocument.all.program[3].value, Session.get('imgBase64')]);
 		//}
 	}
 });
 
 Template.fileUpload.events({
   "change input[type='file']":function(event,template){
-  	console.log(event);
     var files=event.target.files;
     if(files.length===0){
       return;
     }
     var file=files[0];
-    var fileReader=new FileReader();
-    fileReader.onload=function(event){
-      Session.set('imgBase64', event.target.result);
-    };
-    fileReader.readAsDataURL(file);
+    if(file.size > (300*1000)){
+    	return;
+    }else{
+	    var fileReader=new FileReader();
+	    fileReader.onload=function(event){
+	      Session.set('imgBase64', event.target.result);
+	    };
+	    fileReader.readAsDataURL(file);
+	}
   }
 });
