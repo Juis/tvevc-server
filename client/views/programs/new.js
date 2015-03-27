ce5f70459d381067ba591a1f5817f176609d2945
Alisson <alissonplus@msn.com>
2015-03-27 20:55:00 -0300

    Correcao da gravacao do Programa e melhoria de código das Sessions

--- client/views/programs/new.js
+++ client/views/programs/new.js
@@ -1,51 +1,51 @@
 Router.onBeforeAction(function(){
 	this.next();
-	Session.set('getup__i', this.params.query.i);
+	Session.set('getup__get__i', (this.params.query.i)? this.params.query.i : 'undefined');
 });
 
 // PROGRAM NEW PAGE
 Template.programNew.rendered = function () {
 	$('select').material_select();
-	if(Session.get('getup__i') !== 'undefined'){
-		var programSearch = Program.find({_id:Session.get('getup__i'), status:1}).fetch();
+	if(Session.get('getup__get__i') !== 'undefined'){
+		var programSearch = Program.find({_id:Session.get('getup__get__i'), status:1}).fetch();
 		if(programSearch !== ''){
 			document.getElementById("program_id").value = programSearch[0]['_id'];
 			document.getElementById("program_category").value = programSearch[0]['category'];
 			document.getElementById("program_name").value = programSearch[0]['name'];
 			document.getElementById("program_description").value = programSearch[0]['description'];
-			document.getElementById("program_upload").value = programSearch[0]['logo'];
 			document.getElementById("program_imgBase64").src = programSearch[0]['logo'];
+			Session.get('getup__form__imgBase64', programSearch[0]['logo']);
 		}
 	}
 };
 
 Template.programNew.helpers({
 	'title': function(){
-		return (Session.get('getup__i'))? 'Alterar Programa' : 'Novo Programa';
+		return (Session.get('getup__get__i') !== 'undefined')? 'Alterar Programa' : 'Novo Programa';
 	},
 	'imgBase64': function(){
-		return Session.get('imgBase64');
+		return Session.get('getup__form__imgBase64');
 	}
 });
 
 Template.programNew.events({
 	'click a[program]': function(form){
-		if(form.target.ownerDocument.all.program[2].value === '' || form.target.ownerDocument.all.program[3].value === '' || Session.get('imgBase64') === 'undefined'){
+		if(form.target.ownerDocument.all.program[2].value === '' || form.target.ownerDocument.all.program[3].value === '' || Session.get('getup__form__imgBase64') === 'undefined'){
 			toast('Necessário preencher os campos obrigatórios!', 4000);
 		}else{
-			if(!Session.get('getup__i')){
-				Meteor.call('insertProgram', [111, form.target.ownerDocument.all.program[2].value, form.target.ownerDocument.all.program[3].value, form.target.ownerDocument.all.program[4].value, Session.get('imgBase64')]);
+			if(Session.get('getup__get__i') === 'undefined'){
+				Meteor.call('insertProgram', [111, form.target.ownerDocument.all.program[2].value, form.target.ownerDocument.all.program[3].value, form.target.ownerDocument.all.program[4].value, Session.get('getup__form__imgBase64')]);
 				toast('Programa inserido com sucesso.', 4000, 'rounded', 
 					function(){
-						for(var i in form.target.ownerDocument.all.program){
-							form.target.ownerDocument.all.program[i].value = '';
-						}
+						//for(var i in form.target.ownerDocument.all.program){
+						//	form.target.ownerDocument.all.program[i].value = '';
+						//}
 
-						Session.set('imgBase64', 'undefined');
+						//Session.set('getup__form__imgBase64', 'undefined');
 					}
 				);
 			}else{
-				Meteor.call('updateProgram', [222, form.target.ownerDocument.all.program[0].value, form.target.ownerDocument.all.program[2].value, form.target.ownerDocument.all.program[3].value, form.target.ownerDocument.all.program[4].value, Session.get('imgBase64')]);
+				Meteor.call('updateProgram', [222, form.target.ownerDocument.all.program[0].value, form.target.ownerDocument.all.program[2].value, form.target.ownerDocument.all.program[3].value, form.target.ownerDocument.all.program[4].value, Session.get('getup__form__imgBase64')]);
 				toast('Programa atualizado com sucesso.', 4000, 'rounded');
 			}
 		}
@@ -53,11 +53,11 @@ Template.programNew.events({
 });
 
 // SELECT
-Template.programSelect.helpers({
+/*Template.programSelect.helpers({
   "options": function(){
   	return Category.find({status: 1});
   }
-});
+});*/
 
 // FILE UPLOAD
 Template.fileUpload.events({
@@ -72,7 +72,7 @@ Template.fileUpload.events({
     }else{
 	    var fileReader=new FileReader();
 	    fileReader.onload=function(event){
-	      Session.set('imgBase64', event.target.result);
+	      Session.set('getup__form__imgBase64', (event.target.result)? event.target.result : 'undefined');
 	    };
 	    fileReader.readAsDataURL(file);
 	}
@@ -82,6 +82,6 @@ Template.fileUpload.events({
 
 Template.fileUpload.helpers({
 	'imgBase64': function(){
-		return Session.get('imgBase64');
+		return Session.get('getup__form__imgBase64');
 	}
 });
