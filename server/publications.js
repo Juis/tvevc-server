23b0a908b7911b947aea04bfabef0c2faf54d10f
Alisson <alissonplus@msn.com>
2015-03-27 20:57:28 -0300

    removido os campos date e user dos filtros para o usuario

--- server/publications.js
+++ server/publications.js
@@ -1,19 +1,19 @@
 Meteor.publish('programListAllActive', function() {
   //return Program.find({status:1},{fields:{status:0}});
-  return Program.find({status:1});
+  return Program.find({status:1}, {fields:{user_record:0, user_change:0, date_record:0, date_change:0}});
 });
 
 Meteor.publish('programListAllDisabled', function() {
   //return Program.find({status:1},{fields:{status:0}});
-  return Program.find({status:0});
+  return Program.find({status:0}, {fields:{user_record:0, user_change:0, date_record:0, date_change:0}});
 });
 
 Meteor.publish('categoryListAllActive', function() {
   //return Program.find({status:1},{fields:{status:0}});
-  return Category.find({status:1});
+  return Category.find({status:1}, {fields:{user_record:0, user_change:0, date_record:0, date_change:0}});
 });
 
 Meteor.publish('categoryListAllDisabled', function() {
   //return Program.find({status:1},{fields:{status:0}});
-  return Category.find({status:0});
+  return Category.find({status:0}, {fields:{user_record:0, user_change:0, date_record:0, date_change:0}});
 });
