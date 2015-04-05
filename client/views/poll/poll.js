601364e8b2bb9eb76d69f8ad55eb3c27ebff2689
Alisson <alissonplus@msn.com>
2015-04-05 17:35:12 -0300

    Criado o remover das enquetes

--- client/views/poll/poll.js
+++ client/views/poll/poll.js
@@ -1,11 +1,13 @@
- Template.poll.rendered = function(){
-
- //$('.modal-trigger').leanModal();
-
- }
+ Template.poll.rendered = function(){ }
 
  Template.poll.helpers({
 	'polls': function(){
     	return Poll.find({status: 1});
+	}
+});
+
+Template.poll.events({
+	'click #btnDelete': function(form){
+		toastr.warning("Deseja realmente remover a enquete e todas respostas equivalente?<br /><span class=\"btn clear\" onclick=\"Meteor.call('deletePoll', [333, '"+form.currentTarget.childNodes[1].value+"']); $('#toast-container').remove();\">Ok</span><span class=\"btn clear\" onclick=\"$('#toast-container').remove()\">Cancelar</span>", '', {"tapToDismiss": false, "timeOut": 0, "extendedTimeOut": 0});
 	}
 });
