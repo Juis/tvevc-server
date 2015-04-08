0360079f67ad499f31c6d9d2be17c2e0b62ffadf
Alisson <alissonplus@msn.com>
2015-04-07 23:04:02 -0300

    adiconado o crud de notificacoes

--- server/methods.js
+++ server/methods.js
@@ -104,6 +104,29 @@ Meteor.methods({
     }
   },
 
+// NOTIFY
+  'insertNotify': function(data){
+    if(data[0] === 111){
+        Notify.insert({status:1, description:data[1], date_begin:data[2], date_end:data[3], user_record:1, user_change:1, date_record:Meteor.call('dateNow'), date_change:Meteor.call('dateNow')});
+    }else{
+      //erro aqui
+    }
+  },
+
+  'updateNotify': function(data){
+    if(data[0] === 222 && Meteor.call('validateProgram', data)){
+        Notify.update({_id:data[1]},{$set: {description:data[2], date_begin:data[3], date_end:data[4], user_change:1, date_change:Meteor.call('dateNow')}});
+    }else{
+      //erro aqui
+    }
+  },
+
+  'deleteNotify': function(data){
+    if(data[0] === 333){
+      Notify.remove({_id:data[1]});
+    }
+  },
+/*
 //Meteor.call("apiSearch",["program", "97f0ad9e24ca5e0408a269748dgetup"], function(error,result) { console.log(result.content); });
   'apiSearch': function(dataHttp){
     return Meteor.http.call("GET",  "http://alissonplus.meteor.com/collectionapi/"+dataHttp[0], {headers:{"x-auth-token":dataHttp[1]}});   
@@ -127,5 +150,5 @@ Meteor.methods({
 //Meteor.call("apiDeleteOne",["program", "97f0ad9e24ca5e0408a269748dgetup", "dmgcgmrJrdsPtCGez"], function(error,result) { console.log(result.content); });
   'apiDeleteOne': function(dataHttp){
     return Meteor.http.call("DELETE",  "http://alissonplus.meteor.com/collectionapi/"+dataHttp[0]+"/"+dataHttp[2], {headers:{"x-auth-token":dataHttp[1]}});   
-  },
+  },*/
 });
