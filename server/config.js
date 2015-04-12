4dc8b5a98daf4db30eb0e95e2cb5ed6d6e8ebc7b
Alisson <alissonplus@msn.com>
2015-04-11 21:12:14 -0300

    Correção da chamada de consulta do usuário

--- server/config.js
+++ server/config.js
@@ -37,5 +37,5 @@ Meteor.publish('polluser', function() {
 });
 
 Meteor.publish('user', function() {
-  return PollUser.find({status:1}, {fields:{user_record:0, user_change:0, date_record:0, date_change:0}});
+  return User.find({status:1}, {fields:{user_record:0, user_change:0, date_record:0, date_change:0}});
 });
